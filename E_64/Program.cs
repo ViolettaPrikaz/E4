4670c454164d433936b317fd6476f9931e4b668b
Violetta <viopria@gmail.com>
2022-10-03 21:41:59 +0300

    E_64

---
+++ E_64/Program.cs
@@ -0,0 +1,12 @@
+﻿int n = new Random().Next(1, 10); 
+Console.WriteLine(n);
+
+        static string KO(int n)
+{
+        return (n == 1 ? "1" : KO(n - 1) + n);
+}
+ {
+           
+            Console.WriteLine(KO(n));
+ 
+        }
