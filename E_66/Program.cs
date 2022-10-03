67b0be974681d1ee24a5c14f1dc300fc262a61bf
Violetta <viopria@gmail.com>
2022-10-03 21:42:23 +0300

    E_66

---
+++ E_66/Program.cs
@@ -0,0 +1,8 @@
+﻿int n, m, s=0;
+Console.Write("Введи значение n ");
+n = Convert.ToInt16(Console.ReadLine());
+Console.Write("Введи значение m ");
+m = Convert.ToInt16(Console.ReadLine());
+for (int i = n; i <= m; i++)
+    s += i;
+Console.WriteLine(s);
